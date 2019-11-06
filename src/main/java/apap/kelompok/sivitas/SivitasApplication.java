30221e2c98c8e1761176bd43202b350c645d69c5
Winda <windanyaan64@gmail.com>
2019-11-06 17:22:44 +0700

    chore: add gitignore

---
+++ src/main/java/apap/kelompok/sivitas/SivitasApplication.java
@@ -0,0 +1,13 @@
+package apap.kelompok.sivitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SivitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SivitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apap/kelompok/sivitas/SivitasApplicationTests.java
@@ -0,0 +1,16 @@
+package apap.kelompok.sivitas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SivitasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
